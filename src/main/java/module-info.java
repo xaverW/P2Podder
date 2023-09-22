429d13e8305afd9f14054437e524d0f54dcec510
W.Xaver <w.xaver@googlemail.com>
2023-09-22 14:39:36 +0200

    module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,16 @@
 module p2podder {
-
     opens de.p2tools.p2podder;
     exports de.p2tools.p2podder;
-    
-    opens de.p2tools.p2podder.controller.data.podcast;
-    opens de.p2tools.p2podder.controller.data;
 
+    opens de.p2tools.p2podder.controller.data;
+    opens de.p2tools.p2podder.controller.data.podcast;
+    opens de.p2tools.p2podder.controller.data.download;
+    opens de.p2tools.p2podder.controller.data.episode;
+    
     requires de.p2tools.p2lib;
     requires javafx.controls;
     requires org.controlsfx.controls;
+
     requires java.datatransfer;
     requires org.apache.commons.io;
     requires rome;
